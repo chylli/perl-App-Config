ff3d2fa5eb225be70788d56d194b88aaca4f7773
Tom Molesworth <tom@binary.com>
2016-10-31 07:19:46 +0000

    Update version and Changes

--- lib/App/Config/Chronicle.pm
+++ lib/App/Config/Chronicle.pm
@@ -10,11 +10,11 @@ App::Config::Chronicle - An OO configuration module which can be changed and sto
 
 =head1 VERSION
 
-Version 0.01
+Version 0.02
 
 =cut
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 =head1 SYNOPSIS
 
