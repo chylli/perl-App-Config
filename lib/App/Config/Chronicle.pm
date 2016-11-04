4480f248af3f230e01cf98d8091f15ff04955eed
Tom Molesworth <tom@binary.com>
2016-11-04 17:53:10 +0800

    Increase version, 0.02 exists

--- lib/App/Config/Chronicle.pm
+++ lib/App/Config/Chronicle.pm
@@ -10,11 +10,11 @@ App::Config::Chronicle - An OO configuration module which can be changed and sto
 
 =head1 VERSION
 
-Version 0.02
+Version 0.03
 
 =cut
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 =head1 SYNOPSIS
 
