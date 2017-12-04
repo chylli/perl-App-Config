ca0196555b6bb8cb060ad53b13d82c317653237c
Chylli <chylli@binary.com>
2017-12-04 17:23:36 +0800

    typo

--- lib/App/Config/Chronicle/Attribute.pm
+++ lib/App/Config/Chronicle/Attribute.pm
@@ -31,7 +31,7 @@ sub _check_type {
     my $def = $self->{definition};
     $self->{_json_string} //= $def->{isa} eq 'json_string' ? 1 : 0;
     if ($self->{_json_string}) {
-        try { $value = JSON->MaybeXS->new->decode(Encode::decode_utf8($value)) } catch { die "Couldn't decode JSON attribute $value: $_" };
+        try { $value = JSON::MaybeXS->new->decode(Encode::decode_utf8($value)) } catch { die "Couldn't decode JSON attribute $value: $_" };
     } else {
         $self->{_type_constraint} //= find_type_constraint($def->{isa});
         unless ($self->{_type_constraint}) {
